0becf25cb22a90a8e86c912b0cdaaa6491776ebb
Kim Mansfield <kmansfie@yahoo.com>
2019-01-31 15:40:51 -0700

    Try to eliminate all the output

--- test.sh
+++ test.sh
@@ -10,7 +10,7 @@ echo Hello World
 #do
 for i in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20
 do
-	curl -socks5 localhost:1080 http://www.google.com &
+	curl -socks5 localhost:1080 http://www.google.com >/dev/null &
 done
 sleep 10s
 #sleep 90s
@@ -20,7 +20,7 @@ processes=`ps aux | grep apsi | wc | cut -d" " -f7`
 
 if [ "$processes" -gt "4" ]; then
 	echo "Error: bug #22 still there"
-	./runKillAll
+	./runKillAll >/dev/null
 	exit 1
 fi
 
@@ -28,6 +28,6 @@ echo "bug #22 test complete"
 
 #done
 
-./runKillAll
+./runKillAll >/dev/null
 
 exit 0
