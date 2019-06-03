8023c42d9e7252467feddc9e2f88ecdfa6bc00c5
Lester Lecong <lester_lecong@yahoo.com>
2019-06-03 11:56:23 +0800

    Edit curl command.

--- testit.sh
+++ testit.sh
@@ -4,5 +4,5 @@ echo Hello World
 
 for i in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20
 do
-	curl -socks5 localhost:1080 google.com &
+	curl --socks5-hostname localhost:1080 google.com &
 done
