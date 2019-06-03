7a791007c98e241e1eeced606fb8bb4f286fef89
Lester Lecong <lester_lecong@yahoo.com>
2019-06-03 12:08:48 +0800

    Add cleanup and running server.

--- testit.sh
+++ testit.sh
@@ -1,8 +1,21 @@
 #!/bin/sh
 
-echo Hello World
+echo 'Hello World'
+
+cleanup()
+{
+	ssh $USER${SERVER[0]} $TEST_DIR/./runKillAll >/dev/null 2>&1 &
+	ssh $USER${CLIENT[0]} $TEST_DIR/./runKillAll >/dev/null 2>&1 &
+	return $?
+}
+
+
+
+ssh $USER${SERVER[0]} workspace/apsiTests/./runServer >/dev/null 2>&1 &
 
 for i in 1 2 3 4 5 6 7 8 9 10 11 12 13 14 15 16 17 18 19 20
 do
 	curl --socks5-hostname localhost:1080 google.com &
 done
+
+cleanup
